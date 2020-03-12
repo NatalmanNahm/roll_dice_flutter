bb35bc0654e89def5d204793d5aaacd75be79b67
Natalman <natalnahm@gmail.com>
2020-03-12 18:51:14 -0500

    Added a function that update when dice is pressed.

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,17 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
+
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+
+  void pressedDice(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -32,10 +41,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: (){
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                  rightDiceNumber =Random().nextInt(6) + 1;
-                });
+                pressedDice();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -43,10 +49,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: (){
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                  rightDiceNumber = Random().nextInt(6) + 1;
-                });
+                pressedDice();
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
